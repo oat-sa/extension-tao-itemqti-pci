5a8fa48ef49622f5fc2d28f2079dbc7265c9c387
ssipasseuth <sam@taotesting.com>
2014-08-27 15:57:19 +0200

    CreatorPackageParserTest added

--- model/CreatorPackageParser.php
+++ model/CreatorPackageParser.php
@@ -19,14 +19,15 @@
  * 
  */
 
-namespace oat\taoQtiItem\model\qti;
+namespace oat\qtiItemPci\model;
 
 use oat\taoQtiItem\model\qti\PackageParser;
 use oat\taoQtiItem\helpers\QtiPackage;
+use common_Exception;
 use \ZipArchive;
 
 /**
- * The hook used in the item creator
+ * Parser of a QTI PCI Creator package
  *
  * @package qtiItemPci
  */
@@ -43,29 +44,50 @@ class CreatorPackageParser extends PackageParser
     public function validate($schema = ''){
 
         $this->valid = false;
-        
+
         try{
-            
+
             if(QtiPackage::isValidZip($this->source)){
 
                 $zip = new ZipArchive();
                 $zip->open($this->source, ZIPARCHIVE::CHECKCONS);
                 if($zip->locateName("pciCreator.json") === false){
-                    throw new Exception("A PCI creator package must contains a pciCreator.json file at the root of the archive");
-                }else if($zip->locateName("pciCreator.js") === false){
-                    throw new Exception("A PCI creator package must contains a pciCreator.js file at the root of the archive");
-                }else{
-                    //check manifest format :
                     
+                    throw new common_Exception("A PCI creator package must contains a pciCreator.json file at the root of the archive");
+                    
+                }else if($zip->locateName("pciCreator.js") === false){
+                    
+                    throw new common_Exception("A PCI creator package must contains a pciCreator.js file at the root of the archive");
+                    
+                }else{
+                    
+                    //check manifest format :
+                    $manifest = $this->getManifest();
+                    if(!isset($manifest['id'])){
+                        throw new common_Exception('missing required attribute in the manifest pciCreator.json : "id"');
+                    }
                     $this->valid = true;
                 }
 
                 $zip->close();
             }
-            
-        }catch(Exception $e){
+        }catch(common_Exception $e){
             $this->addError($e);
         }
+    }
+
+    public function getManifest(){
+        
+        $str = '';
+        $handle = fopen('zip://'.$this->source.'#pciCreator.json', 'r');
+        while(!feof($handle)){
+            $str .= fread($handle, 8192);
+        }
+        fclose($handle);
+        
+        $returnValue = json_decode($str, true);
+        
+        return $returnValue;
     }
 
 }
