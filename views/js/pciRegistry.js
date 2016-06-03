bc1891ae9cebc1d5c72be8e617b8d14c44aeb57d
ssipasseuth <sam@taotesting.com>
2016-06-03 14:08:15 +0200

    eventifier pciRegistry

--- views/js/pciRegistry.js
+++ views/js/pciRegistry.js
@@ -16,161 +16,158 @@
  * Copyright (c) 2016 (original work) Open Assessment Technlogies SA;
  *
  */
-define(['jquery', 'lodash', 'core/promise'], function ($, _, Promise){
+define(['lodash', 'core/promise', 'core/eventifier'], function (_, Promise, eventifier){
     'use strict';
 
-    var _requirejs = window.require;
-    var _loaded = false;
-    var _providers = [];
-    var _registry = {};
+    function pciRegistryFactory(){
+        
+        var _requirejs = window.require;
+        var _loaded = false;
+        var _providers = [];
+        var _registry = {};
 
-    function getAllVersions(){
-        var all = {};
-        _.forIn(_registry, function (versions, id){
-            all[id] = _.map(versions, 'version');
-        });
-        return all;
-    }
+        function _get(typeIdentifier, version){
 
-    function _get(typeIdentifier, version){
-
-        if(_registry[typeIdentifier]){
-            //check version
-            if(version){
-                return _.find(_registry[typeIdentifier], version);
-            }else{
-                //latest
-                return _.last(_registry[typeIdentifier]);
+            if(_registry[typeIdentifier]){
+                //check version
+                if(version){
+                    return _.find(_registry[typeIdentifier], version);
+                }else{
+                    //latest
+                    return _.last(_registry[typeIdentifier]);
+                }
             }
         }
-    }
 
-    function getRuntime(typeIdentifier, version){
-        var pci = _get(typeIdentifier, version);
-        if(pci){
-            return _.assign(pci.runtime, {baseUrl : pci.baseUrl});
-        }else{
-            throw 'no pci found';
-        }
-    }
-
-    function getCreator(typeIdentifier, version){
-        var pci = _get(typeIdentifier, version);
-        if(pci && pci.creator){
-            return _.assign(pci.creator, {
-                baseUrl : pci.baseUrl,
-                response : pci.response
-            });
-        }else{
-            throw 'no pci found';
-        }
-    }
-
-    function getBaseUrl(typeIdentifier, version){
-        var runtime = _get(typeIdentifier, version);
-        if(runtime){
-            return runtime.baseUrl;
-        }
-        return '';
-    }
-    
-    function addProvider(provider){
-        if(provider && _.isFunction(provider.load)){
-            _providers.push(provider);
-        }
-        return this;
-    }
-    
-    function loadRuntimes(callback, reload){
-        
-        var loadStack = [];
-        
-        if(_loaded && !reload){
-            callback();
-        }else{
-            
-            _.each(_providers, function(provider){
-                loadStack.push(provider.load());
-            });
-            
-            //performs the loadings in parrallel
-            Promise.all(loadStack).then(function(results){
-                
-                var requireConfigAliases = {};
-                
-                //update registry
-                _registry = _.reduce(results, function(acc, _pcis){
-                    return _.merge(acc, _pcis);
-                }, {});
-
-                //preconfiguring the pci's code baseUrl
-                _.forIn(_registry, function (versions, typeIdentifier){
-                    //currently use latest runtime path
-                    requireConfigAliases[typeIdentifier] = getBaseUrl(typeIdentifier);
+        return eventifier({
+            addProvider : function addProvider(provider){
+                if(provider && _.isFunction(provider.load)){
+                    _providers.push(provider);
+                }
+                return this;
+            },
+            getAllVersions : function getAllVersions(){
+                var all = {};
+                _.forIn(_registry, function (versions, id){
+                    all[id] = _.map(versions, 'version');
                 });
-                _requirejs.config({paths : requireConfigAliases});
+                return all;
+            },
+            getRuntime : function getRuntime(typeIdentifier, version){
+                var pci = _get(typeIdentifier, version);
+                if(pci){
+                    return _.assign(pci.runtime, {baseUrl : pci.baseUrl});
+                }else{
+                    throw 'no pci found';
+                }
+            },
+            getCreator : function getCreator(typeIdentifier, version){
+                var pci = _get(typeIdentifier, version);
+                if(pci && pci.creator){
+                    return _.assign(pci.creator, {
+                        baseUrl : pci.baseUrl,
+                        response : pci.response
+                    });
+                }else{
+                    throw 'no pci found';
+                }
+            },
+            getAuthoringData : function getAuthoringData(typeIdentifier, version){
+                var pciModel = _get(typeIdentifier, version);
+                if(pciModel && pciModel.creator){
+                    return {
+                        label : pciModel.label, //currently no translation available 
+                        icon : pciModel.creator.icon.replace(new RegExp('^' + typeIdentifier + '\/'), pciModel.baseUrl),
+                        short : pciModel.short,
+                        description : pciModel.description,
+                        qtiClass : 'customInteraction.' + pciModel.typeIdentifier, //custom interaction is block type
+                        tags : _.union(['Custom Interactions'], pciModel.tags)
+                    };
+                }
+            },
+            getBaseUrl : function getBaseUrl(typeIdentifier, version){
+                var runtime = _get(typeIdentifier, version);
+                if(runtime){
+                    return runtime.baseUrl;
+                }
+                return '';
+            },
+            loadRuntimes : function loadRuntimes(callback, reload){
 
-                _loaded = true;
+                var self = this;
+                var loadStack = [];
+
+                if(_loaded && !reload){
+                    callback();
+                    self.trigger('runtimesloaded');
+                }else{
+
+                    _.each(_providers, function (provider){
+                        loadStack.push(provider.load());
+                    });
+
+                    //performs the loadings in parrallel
+                    Promise.all(loadStack).then(function (results){
+
+                        var requireConfigAliases = {};
+
+                        //update registry
+                        _registry = _.reduce(results, function (acc, _pcis){
+                            return _.merge(acc, _pcis);
+                        }, {});
+
+                        //preconfiguring the pci's code baseUrl
+                        _.forIn(_registry, function (versions, typeIdentifier){
+                            //currently use latest runtime path
+                            requireConfigAliases[typeIdentifier] = self.getBaseUrl(typeIdentifier);
+                        });
+                        _requirejs.config({paths : requireConfigAliases});
+
+                        _loaded = true;
+                        
+                        callback();
+                        self.trigger('runtimesloaded');
+                        
+                    }).catch(function (err){
+                        
+                        self.trigger('error', err);
+                    });
+                }
+            },
+            loadCreators : function loadCreators(callback, reload){
                 
-                //@todo eventify it and fires event
-                callback();
-            }).catch(function(err){
-//                self.trigger('error', err);
-            });
-        }
-    }
-    
-    function loadCreators(callback, reload){
+                var self = this;
+                this.loadRuntimes(function (){
+                    var requiredCreators = [];
 
-        loadRuntimes(function (){
-            var requiredCreators = [];
+                    _.forIn(_registry, function (versions, typeIdentifier){
+                        var pciModel = _get(typeIdentifier);//currently use the latest version only
+                        requiredCreators.push(pciModel.creator.hook.replace(/\.js$/, ''));
+                    });
 
-            _.forIn(_registry, function (versions, typeIdentifier){
-                var pciModel = _get(typeIdentifier);//currently use the latest version only
-                requiredCreators.push(pciModel.creator.hook.replace(/\.js$/, ''));
-            });
-
-            //@todo support caching
-            _requirejs(requiredCreators, function (){
-                var creators = {};
-                _.each(arguments, function (creatorHook){
-                    var id = creatorHook.getTypeIdentifier();
-                    var pciModel = _get(id);
-                    var i = _.findIndex(_registry[id], {version : pciModel.version});
-                    if(i < 0){
-                        throw 'no creator found for id/version ' + id + '/' + pciModel.version;
-                    }else{
-                        _registry[id][i].creator.module = creatorHook;
-                        creators[id] = creatorHook;
-                    }
-                });
-                callback(creators);
-            });
-        }, reload);
-    }
-
-    function getAuthoringData(typeIdentifier, version){
-        var pciModel = _get(typeIdentifier, version);
-        if(pciModel && pciModel.creator){
-            return {
-                label : pciModel.label, //currently no translation available 
-                icon : pciModel.creator.icon.replace(new RegExp('^' + typeIdentifier + '\/'), pciModel.baseUrl),
-                short : pciModel.short,
-                description : pciModel.description,
-                qtiClass : 'customInteraction.' + pciModel.typeIdentifier, //custom interaction is block type
-                tags : _.union(['Custom Interactions'], pciModel.tags)
-            };
-        }
-    }
-
-    return {
-        addProvider : addProvider,
-        getAllVersions : getAllVersions,
-        getRuntime : getRuntime,
-        getCreator : getCreator,
-        getAuthoringData : getAuthoringData,
-        getBaseUrl : getBaseUrl,
-        load : loadRuntimes,
-        loadCreators : loadCreators
+                    //@todo support caching
+                    _requirejs(requiredCreators, function (){
+                        var creators = {};
+                        _.each(arguments, function (creatorHook){
+                            var id = creatorHook.getTypeIdentifier();
+                            var pciModel = _get(id);
+                            var i = _.findIndex(_registry[id], {version : pciModel.version});
+                            if(i < 0){
+                                throw 'no creator found for id/version ' + id + '/' + pciModel.version;
+                            }else{
+                                _registry[id][i].creator.module = creatorHook;
+                                creators[id] = creatorHook;
+                            }
+                        });
+                        callback(creators);
+                        self.trigger('runtimesloaded');
+                    });
+                }, reload);
+                
+            }
+        });
     };
+    
+    //singleton
+    return pciRegistryFactory();
 });
