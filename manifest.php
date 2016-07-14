6e92b639a29432f1cb335a395674a440f1029ea6
Sam Sipasseuth <sam@taotesting.com>
2016-07-14 11:28:47 +0200

    bump version

--- manifest.php
+++ manifest.php
@@ -30,7 +30,7 @@ return array(
     'license' => 'GPL-2.0',
     'version' => '0.2.0',
 	'author' => 'Open Assessment Technologies SA',
-	'requires' => array('taoQtiItem' => '>=2.7.0'),
+	'requires' => array('taoQtiItem' => '>=3.1.0'),
     'acl' => array(
         array('grant', 'http://www.tao.lu/Ontologies/generis.rdf#qtiItemPciManager', array('ext'=>'qtiItemPci')),
 		array('grant', 'http://www.tao.lu/Ontologies/TAOItem.rdf#QTIManagerRole', array('ext'=>'qtiItemPci', 'mod' => 'PciLoader')),
@@ -46,7 +46,7 @@ return array(
             SetupPciRegistry::class,
 			SetQtiCreatorConfig::class,
 			RegisterClientProvider::class,
-//			RegisterPci::class
+			RegisterPci::class
 		)
     ),
     'uninstall' => array(
