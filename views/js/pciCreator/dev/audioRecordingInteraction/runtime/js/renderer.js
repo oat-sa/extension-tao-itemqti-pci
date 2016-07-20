4f891ac28c8207c20bcfe211b4661db6eeef80c3
Christophe Noël <christophe@taotesting.com>
2016-07-20 14:50:12 +0200

    clean up example

--- views/js/pciCreator/dev/audioRecordingInteraction/runtime/js/renderer.js
+++ views/js/pciCreator/dev/audioRecordingInteraction/runtime/js/renderer.js
@@ -1,40 +1,17 @@
 define([
     'IMSGlobal/jquery_2_1_1',
     'OAT/util/html',
-    //todo: remove this! use the packaged version as a shared library of the pci, or use a custom player
-    'ui/mediaplayer',
-    'audioRecordingInteraction/runtime/js/recorder'
-], function($, html, mediaplayer, recorder){
+], function($, html){
     'use strict';
 
-    // function renderChoices(id, $container, config){
-    //
-    //     var $li,
-    //         level = parseInt(config.level) || 5,
-    //         $ul = $container.find('ul.likert');
-    //
-    //     ensure that renderChoices() is idempotent
-        // $ul.empty();
-        //
-        // add levels
-        // for(var i = 1; i <= level; i++){
-        //
-        //     $li = $('<li>', {'class' : 'likert'});
-        //     $li.append($('<input>', {type : 'radio', name : id, value : i}));
-        //
-        //     $ul.append($li);
-        // }
-    // }
-    //
-    // function renderLabels(id, $container, config){
-    //
-    //     var $ul = $container.find('ul.likert');
-    //     var $labelMin = $('<span>', {'class' : 'likert-label likert-label-min'}).html(config['label-min']);
-    //     var $labelMax = $('<span>', {'class' : 'likert-label likert-label-max'}).html(config['label-max']);
-    //
-    //     $ul.before($labelMin);
-    //     $ul.after($labelMax);
-    // }
+    function getUserMedia() {
+        //todo: complete this with proper error handling
+        //todo: see https://github.com/otalk/getUserMedia
+        return (navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
+    }
 
     return {
         render : function(id, container, config){
@@ -43,14 +20,6 @@ define([
 
             var $recorderContainer = $container.find('.audioRec > .recorder');
             var $playerContainer = $container.find('.audioRec > .player');
-
-            // var player = mediaplayer({
-            //     renderTo: $playerContainer,
-            //     url: 'tbd',
-                // loop: false,
-                // canPause: true,
-                // height: 5
-            // });
 
             var $startButton = $('<button>', {
                 text: 'Start recording'
@@ -63,52 +32,30 @@ define([
             $recorderContainer.append($startButton);
             $recorderContainer.append($stopButton);
 
-// fork getUserMedia for multiple browser versions, for those
-// that need prefixes
-            navigator.getUserMedia = (navigator.getUserMedia ||
-                    navigator.webkitGetUserMedia ||
-                    navigator.mozGetUserMedia ||
-                    navigator.msGetUserMedia);
-
-// define other variables
+            // example
 
             var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
             var audio = document.querySelector('audio');
 
-// getUserMedia block - grab stream
-// put it into a MediaStreamAudioSourceNode
-
-            if (navigator.getUserMedia) {
-                console.log('getUserMedia supported.');
+                // getUserMedia block - grab stream
+                // put it into a MediaStreamAudioSourceNode
+                navigator.getUserMedia = getUserMedia();
                 navigator.getUserMedia (
-                    // constraints: audio and video for this app
-                    {
-                        audio: true
-                    },
+                    { audio: true },
 
-                    // Success callback
                     function(stream) {
                         audio.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
-                        audio.onloadedmetadata = function(e) {
-                            audio.play();
-                            audio.muted = 'true';
-                        };
+                        // audio.onloadedmetadata = function(e) {
+                        //     audio.play();
+                        //     audio.muted = 'true';
+                        // };
 
                         // Create a MediaStreamAudioSourceNode
                         // Feed the HTMLMediaElement into it
                         var source = audioCtx.createMediaStreamSource(stream);
 
-                        // Create a biquadfilter
-                        var biquadFilter = audioCtx.createBiquadFilter();
-                        biquadFilter.type = "lowshelf";
-                        biquadFilter.frequency.value = 1000;
-                        biquadFilter.gain.value = 20;
-
-                        // connect the AudioBufferSourceNode to the gainNode
-                        // and the gainNode to the destination, so we can play the
-                        // music and adjust the volume using the mouse cursor
-                        source.connect(biquadFilter);
-                        biquadFilter.connect(audioCtx.destination);
+                        // connect the AudioBufferSourceNode to the destination
+                        source.connect(audioCtx.destination);
 
                     },
 
@@ -117,11 +64,8 @@ define([
                         console.log('The following gUM error occured: ' + err);
                     }
                 );
-            } else {
-                console.log('getUserMedia not supported on your browser!');
-            }
 
-// dump script to pre element
+
 
 
 
