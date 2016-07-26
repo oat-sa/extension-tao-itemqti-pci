4e4e79c13af13fd20f6bf1b3a3d64d7898e1bfda
Christophe Noël <christophe@taotesting.com>
2016-07-26 14:14:42 +0200

    first control behaviors

--- views/js/pciCreator/dev/audioRecordingInteraction/runtime/js/renderer.js
+++ views/js/pciCreator/dev/audioRecordingInteraction/runtime/js/renderer.js
@@ -12,13 +12,18 @@ define([
 
     var _response = {};
 
-    setGetUserMedia();
-
     return {
         getResponse : function getResponse() {
             return _response;
         },
 
+        /**
+         *
+         * @param id
+         * @param container
+         * @param config
+         * config.
+         */
         render : function(id, container, config) {
 
             var $container = $(container);
@@ -26,8 +31,20 @@ define([
             var controls = {},
                 $controlsContainer = $container.find('.audioRec > .controls');
 
+            config = _.defaults(config, {
+                audioBitrate: 20000,
+                allowPlayback: true,
+                autostart: false,
+                maxRecords: -1,
+                maxRecordingTime: 10
+            });
 
-            initializeControls();
+
+
+
+
+
+
 
             var $recorderContainer = $container.find('.audioRec > .recorder');
             var $playerContainer = $container.find('.audioRec > .player');
@@ -80,8 +97,133 @@ define([
 
             // getUserMedia block - grab stream
             // put it into a MediaStreamAudioSourceNode
+
+
+            function playerFactory() {
+                var audioEl;
+                return {
+                    load: function(url) {
+                        audioEl = new Audio(url);
+                    },
+                    play: function() {
+                        audioEl.play();
+                    }
+                };
+            }
+
+
+
+            setGetUserMedia();
             navigator.mediaDevices.getUserMedia ({ audio: true })
                 .then(function(stream) {
+                    return recorderFactory(stream, config);
+                })
+                .then(function(recorder) {
+
+                    var player = playerFactory();
+
+                    initializeControls();
+
+                    recorder.ondataavailable = function(e) {
+                        // todo: add checks on createObjectURL ?
+                        var blob = e.data;
+                        var blobUrl = URL.createObjectURL(blob);
+
+                        controls.record.disable();
+                        controls.stop.disable();
+                        controls.play.enable();
+                        controls.reset.enable();
+
+                        endRecording(e.data);
+                        player.load(blobUrl);
+                    };
+
+                    function startRecording() {
+                        controls.record.activate();
+                        controls.stop.enable();
+                        controls.play.disable();
+                        controls.reset.disable();
+
+                        recorder.start();
+                    }
+
+                    function stopRecording() {
+                        controls.record.disable();
+                        controls.stop.disable();
+                        controls.play.disable();
+                        controls.reset.disable();
+
+                        recorder.stop();
+                    }
+
+                    function playRecording() {
+                        controls.record.disable();
+                        controls.stop.enable();
+                        controls.play.activate();
+                        controls.reset.disable();
+
+                        player.play();
+                    }
+
+
+
+                    function endRecording(blob) {
+                        var blobURL = URL.createObjectURL(blob);
+                        audio.src = blobURL;
+
+                        var downloadLink = document.createElement('a');
+                        document.body.appendChild(downloadLink);
+                        downloadLink.text =
+                            'download ' + profile.id + ' - ' + blob.type + ' : ' + Math.round((blob.size / 1000)) + 'KB';
+                        downloadLink.download =
+                            browser + ' - ' +
+                            encoder + ' - ' +
+                            profile.id + ' - ' + Date.now() +
+                            '.' + blob.type.split('/')[1];
+                        downloadLink.href = blobURL;
+
+                        var reader = new FileReader();
+                        reader.readAsDataURL(blob);
+
+                        reader.onloadend = function onLoadEnd(e) {
+                            var filename = 'audioRecording' + Date.now() +
+                                '.' + blob.type.split('/')[1];
+
+                            var base64Data = e.target.result;
+                            var commaPosition = base64Data.indexOf(',');
+                            var base64Raw = base64Data.substring(commaPosition + 1);
+
+                            // Store the base64 encoded data for later use.
+
+                            _response = {"base": {"file": {"data": base64Raw, "mime": blob.type, "name": filename}}};
+
+                            console.dir(_response);
+                        };
+                        // var base64Raw = base64Data;
+                        //
+                        // var commaPosition = base64Data.indexOf(',');
+                        // var base64Raw = base64Data.substring(commaPosition + 1);
+                        // _response = {"base": {"file": {"data": base64Raw, "mime": blob.type, "name": filename}}};
+
+                        // from file upload interaction
+                        /*
+                         var base64Data = e.target.result;
+                         var commaPosition = base64Data.indexOf(',');
+
+                         // Store the base64 encoded data for later use.
+
+                         _response = {"base": {"file": {"data": base64Raw, "mime": filetype, "name": filename}}};
+                         */
+                    }
+
+
+                    // function playRecording() {
+                    //     co
+                    // }
+
+                    // ===========================================
+                    // START EXPERIMENTS
+                    // ===========================================
 
                     var mediaRecorder;
 
@@ -308,107 +450,106 @@ define([
                         $recordingState.text('stopped');
                     }
 
+                    // ===========================================
+                    // END EXPERIMENTS
+                    // ===========================================
+
+
+
+
+
+                    function initializeControls() {
+                        controls.record = newControl({
+                            defaultState: 'enabled',
+                            label: 'record',
+                            // icon: 'radio-bg',
+                            display: true,
+                            container: $controlsContainer,
+                            onclick: function onclick() {
+                                startRecording();
+                            }
+                        });
+
+                        controls.stop = newControl({
+                            defaultState: 'disabled',
+                            label: 'stop',
+                            // icon: 'stop',
+                            display: true,
+                            container: $controlsContainer,
+                            onclick: function onclick() {
+                                stopRecording();
+                            }
+                        });
+
+                        controls.play = newControl({
+                            defaultState: 'disabled',
+                            label: 'play',
+                            // icon: 'play',
+                            display: true,
+                            container: $controlsContainer,
+                            onclick: function onclick() {
+                                playRecording();
+                            }
+                        });
+
+                        controls.reset = newControl({
+                            defaultState: 'disabled',
+                            label: 'reset',
+                            // icon: 'loop',
+                            display: true,
+                            container: $controlsContainer,
+                            onclick: function onclick() {
+                                // resetRecording();
+                            }
+                        });
+
+                    }
+
+                    /**
+                     * //todo: add jsdoc
+                     * @param options
+                     * @returns {{enable: enable, disable: disable, activate: activate}}
+                     */
+                    function newControl(options) {
+                        var state;
+                        var $control = $(controlTpl({
+                            label: options.label
+                        }));
+                        $control.on('click', function() {
+                            if (state === 'enabled') {
+                                options.onclick();
+                            }
+                        });
+                        if (options.display === true) {
+                            $control.appendTo(options.container);
+                        }
+                        setState(options.defaultState || 'enabled');
+
+                        function setState(newState) {
+                            state = newState;
+                            $control.removeClass('disabled enabled active');
+                            $control.addClass(newState);
+                        }
+
+                        return {
+                            enable: function() {
+                                setState('enabled');
+                            },
+                            disable: function() {
+                                setState('disabled');
+                            },
+                            activate: function() {
+                                setState('active');
+                            }
+                        };
+                    }
+
+
                 })
                 .catch(function(err) {
-                    console.log('The following gUM error occured: ');
-                    console.dir(err);
+                    throw err;
                 });
 
-
-
-
-
-
-
-
-
-
-
-            function initializeControls() {
-                controls.record = newControl({
-                    defaultState: 'enabled',
-                    label: 'record',
-                    // icon: 'radio-bg',
-                    display: true,
-                    container: $controlsContainer,
-                    onclick: function onclick() {
-                        // startRecording();
-                    }
-                });
-
-                controls.stop = newControl({
-                    defaultState: 'disabled',
-                    label: 'stop',
-                    // icon: 'stop',
-                    display: true,
-                    container: $controlsContainer,
-                    onclick: function onclick() {
-                        // stopRecording();
-                    }
-                });
-
-                controls.play = newControl({
-                    defaultState: 'active',
-                    label: 'play',
-                    // icon: 'play',
-                    display: true,
-                    container: $controlsContainer,
-                    onclick: function onclick() {
-                        // playRecording();
-                    }
-                });
-
-                controls.stop = newControl({
-                    defaultState: 'disabled',
-                    label: 'reset',
-                    // icon: 'loop',
-                    display: true,
-                    container: $controlsContainer,
-                    onclick: function onclick() {
-                        // resetRecording();
-                    }
-                });
-
-            }
-
-            /**
-             * //todo: add jsdoc
-             * @param options
-             * @returns {{enable: enable, disable: disable, activate: activate}}
-             */
-            function newControl(options) {
-                var state;
-                var $control = $(controlTpl({
-                    label: options.label
-                }));
-                $control.on('click', function onRecord() {
-                    if (state === 'enabled') {
-                        options.onclick();
-                    }
-                });
-                if (options.display === true) {
-                    $control.appendTo(options.container);
-                }
-                setState(options.defaultState || 'enabled');
-
-                function setState(newState) {
-                    state = newState;
-                    $control.removeClass('disabled', 'enabled', 'active');
-                    $control.addClass(newState);
-                }
-
-                return {
-                    enable: function() {
-                        setState('enabled');
-                    },
-                    disable: function() {
-                        setState('disabled');
-                    },
-                    activate: function() {
-                        setState('active');
-                    }
-                };
-            }
 
 
             //render rich text content in prompt
@@ -421,6 +562,59 @@ define([
         // }
     };
 
+    /**
+     * todo: jsdoc
+     * Minimal MediaRecorder wrapper in case an alternate recorder implementation is needed someday
+     * for incompatible browsers
+     * @param stream
+     */
+    function recorderFactory(stream, config) {
+        return new Promise(function (resolve, reject) {
+            var mediaRecorder,
+                mediaRecorderWrapper,
+                recorderOptions = {
+                    audioBitsPerSecond: config.audioBitrate
+                };
+
+            if (typeof MediaRecorder === 'undefined') {
+                reject(new Error('MediaRecorder API not supported. Please use a compatible browser'));
+            }
+
+            // Prefered encoding format order:
+            // webm/opus, ogg/opus, webm, ogg, default
+            if (typeof MediaRecorder.isTypeSupported === 'function') {
+                if (MediaRecorder.isTypeSupported('audio/webm;codecs=opus')) {
+                    recorderOptions.mimeType = 'audio/webm;codecs=opus';
+                } else if (MediaRecorder.isTypeSupported('audio/ogg;codecs=opus')) {
+                    recorderOptions.mimeType = 'audio/ogg;codecs=opus';
+                } else if (MediaRecorder.isTypeSupported('audio/webm')) {
+                    recorderOptions.mimeType = 'audio/webm';
+                } else if (MediaRecorder.isTypeSupported('audio/ogg')) {
+                    recorderOptions.mimeType = 'audio/ogg';
+                }
+            }
+            mediaRecorder = new MediaRecorder(stream, recorderOptions);
+
+            mediaRecorder.ondataavailable = function (e) {
+                if (mediaRecorderWrapper.ondataavailable) {
+                    mediaRecorderWrapper.ondataavailable(e);
+                }
+            };
+
+            mediaRecorderWrapper = {
+                start: function() {
+                    mediaRecorder.start();
+                },
+
+                stop: function() {
+                    mediaRecorder.stop();
+                }
+            };
+
+            resolve(mediaRecorderWrapper);
+        });
+    }
+
 
     //todo: check licence or rewrite
     // MediaDevices.getUserMedia polyfill
@@ -432,9 +626,9 @@ define([
 
             // First get ahold of getUserMedia, if present
             var getUserMedia = (navigator.getUserMedia ||
-            navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia ||
-            navigator.msGetUserMedia);
+                navigator.webkitGetUserMedia ||
+                navigator.mozGetUserMedia ||
+                navigator.msGetUserMedia);
 
             // Some browsers just don't implement it - return a rejected promise with an error
             // to keep a consistent interface
