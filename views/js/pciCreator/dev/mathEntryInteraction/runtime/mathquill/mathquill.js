e670168ec9c3f100baee8600387301a24cc38847
Christophe Noël <christophe@taotesting.com>
2016-10-14 16:39:57 +0200

    added comment about a change made in mathquill source code

--- views/js/pciCreator/dev/mathEntryInteraction/runtime/mathquill/mathquill.js
+++ views/js/pciCreator/dev/mathEntryInteraction/runtime/mathquill/mathquill.js
@@ -6,6 +6,10 @@
  * Mozilla Public License, v. 2.0. If a copy of the MPL
  * was not distributed with this file, You can obtain
  * one at http://mozilla.org/MPL/2.0/.
+ *
+ * =====================================================================
+ * OAT WARNING : Line 4934 has been changed from the original source code.
+ * =====================================================================
  */
 
 (function() {
