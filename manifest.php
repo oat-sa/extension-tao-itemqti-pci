d54aed653b962bc7c4ac5bb7e7e8e93b7af7b256
Sam Sipasseuth <sam@taotesting.com>
2017-06-09 13:34:49 +0200

    bump version

--- manifest.php
+++ manifest.php
@@ -32,7 +32,7 @@ return array(
     'label' => 'QTI Portable Custom Interaction',
     'description' => '',
     'license' => 'GPL-2.0',
-    'version' => '2.0.2',
+    'version' => '2.1.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao' => '>=9.0.0',
