2a98bc7d4a58d59e85b9a387e6738c25eae984db
Christophe Noël <christophe@taotesting.com>
2017-06-27 11:39:34 +0200

    added check for mediarecorder support in tests

--- views/js/test/audioRecordingInteraction/test.js
+++ views/js/test/audioRecordingInteraction/test.js
@@ -28,6 +28,16 @@ define([
         }
     });
 
+    // This is for PhantomJS compatibility
+    function supportsMediaRecorder() {
+        if (!window.MediaRecorder) {
+            QUnit.assert.ok(true, 'skipping test...');
+            QUnit.start();
+            return false;
+        }
+        return true;
+    }
+
     /* */
 
     QUnit.asyncTest('renders correctly', function (assert){
@@ -36,20 +46,22 @@ define([
         assert.equal($container.length, 1, 'the item container exists');
         assert.equal($container.children().length, 0, 'the container has no children');
 
-        runner = qtiItemRunner('qti', itemData)
-            .on('render', function (){
+        if (supportsMediaRecorder()) {
+            runner = qtiItemRunner('qti', itemData)
+                .on('render', function (){
 
-                assert.equal($container.children().length, 1, 'the container a elements');
-                assert.equal($container.children('.qti-item').length, 1, 'the container contains a the root element .qti-item');
-                assert.equal($container.find('.qti-interaction').length, 1, 'the container contains an interaction .qti-interaction');
-                assert.equal($container.find('.qti-interaction.qti-customInteraction').length, 1, 'the container contains a custom interaction');
-                assert.equal($container.find('.qti-customInteraction .audioRecordingInteraction').length, 1, 'the container contains a Point Line Graph interaction');
-                assert.equal($container.find('.qti-customInteraction .prompt').length, 1, 'the interaction contains a prompt');
+                    assert.equal($container.children().length, 1, 'the container a elements');
+                    assert.equal($container.children('.qti-item').length, 1, 'the container contains a the root element .qti-item');
+                    assert.equal($container.find('.qti-interaction').length, 1, 'the container contains an interaction .qti-interaction');
+                    assert.equal($container.find('.qti-interaction.qti-customInteraction').length, 1, 'the container contains a custom interaction');
+                    assert.equal($container.find('.qti-customInteraction .audioRecordingInteraction').length, 1, 'the container contains a Point Line Graph interaction');
+                    assert.equal($container.find('.qti-customInteraction .prompt').length, 1, 'the interaction contains a prompt');
 
-                QUnit.start();
-            })
-            .init()
-            .render($container);
+                    QUnit.start();
+                })
+                .init()
+                .render($container);
+        }
     });
 
     /* */
@@ -62,29 +74,31 @@ define([
         assert.equal($container.length, 1, 'the item container exists');
         assert.equal($container.children().length, 0, 'the container has no children');
 
-        runner = qtiItemRunner('qti', itemData)
-            .on('render', function(){
-                var $controls;
+        if (supportsMediaRecorder()) {
+            runner = qtiItemRunner('qti', itemData)
+                .on('render', function(){
+                    var $controls;
 
-                //call destroy manually
-                var interaction = this._item.getInteractions()[0];
-                interaction.renderer.destroy(interaction);
-
-                $controls = $('.audiorec-control', $container);
-                assert.equal($controls.length, 0, 'recorder has been destroyed');
-
-                QUnit.start();
-                try {
+                    //call destroy manually
+                    var interaction = this._item.getInteractions()[0];
                     interaction.renderer.destroy(interaction);
-                } catch(e){
-                    console.log(e);
-                }
-            })
-            .on('error', function (error){
-                $('#error-display').html(error);
-            })
-            .init()
-            .render($container);
+
+                    $controls = $('.audiorec-control', $container);
+                    assert.equal($controls.length, 0, 'recorder has been destroyed');
+
+                    QUnit.start();
+                    try {
+                        interaction.renderer.destroy(interaction);
+                    } catch(e){
+                        console.log(e);
+                    }
+                })
+                .on('error', function (error){
+                    $('#error-display').html(error);
+                })
+                .init()
+                .render($container);
+        }
     });
 
     /* */
@@ -104,37 +118,39 @@ define([
         assert.equal($container.length, 1, 'the item container exists');
         assert.equal($container.children().length, 0, 'the container has no children');
 
-        runner = qtiItemRunner('qti', itemData)
-            .on('render', function (){
-                var interaction,
-                    interactions = this._item.getInteractions();
+        if (supportsMediaRecorder()) {
+            runner = qtiItemRunner('qti', itemData)
+                .on('render', function (){
+                    var interaction,
+                        interactions = this._item.getInteractions();
 
-                assert.equal(_.size(interactions), 1, 'one interaction');
-                interaction = interactions[0];
-
-                // first we set the response
-                interaction.setResponse(response);
-            })
-            .on('responsechange', function (res){
-                var interactions = this._item.getInteractions(),
+                    assert.equal(_.size(interactions), 1, 'one interaction');
                     interaction = interactions[0];
-                changeCounter++;
 
-                if (changeCounter === 1) {
-                    interaction.resetResponse();
-                } else if (changeCounter === 2) {
-                    assert.ok(_.isPlainObject(res), 'response changed');
-                    assert.ok(_.isPlainObject(res.RESPONSE), 'response identifier ok');
-                    assert.ok(_.isPlainObject(res.RESPONSE.base), 'response base is an object');
-                    assert.ok(_.isNull(res.RESPONSE.base.file), 'response record is empty');
-                    QUnit.start();
-                }
-            })
-            .on('error', function (error){
-                $('#error-display').html(error);
-            })
-            .init()
-            .render($container);
+                    // first we set the response
+                    interaction.setResponse(response);
+                })
+                .on('responsechange', function (res){
+                    var interactions = this._item.getInteractions(),
+                        interaction = interactions[0];
+                    changeCounter++;
+
+                    if (changeCounter === 1) {
+                        interaction.resetResponse();
+                    } else if (changeCounter === 2) {
+                        assert.ok(_.isPlainObject(res), 'response changed');
+                        assert.ok(_.isPlainObject(res.RESPONSE), 'response identifier ok');
+                        assert.ok(_.isPlainObject(res.RESPONSE.base), 'response base is an object');
+                        assert.ok(_.isNull(res.RESPONSE.base.file), 'response record is empty');
+                        QUnit.start();
+                    }
+                })
+                .on('error', function (error){
+                    $('#error-display').html(error);
+                })
+                .init()
+                .render($container);
+        }
     });
 
     /* */
@@ -154,31 +170,33 @@ define([
         assert.equal($container.length, 1, 'the item container exists');
         assert.equal($container.children().length, 0, 'the container has no children');
 
-        runner = qtiItemRunner('qti', itemData)
-            .on('render', function (){
-                var interaction,
-                    interactions = this._item.getInteractions();
+        if (supportsMediaRecorder()) {
+            runner = qtiItemRunner('qti', itemData)
+                .on('render', function (){
+                    var interaction,
+                        interactions = this._item.getInteractions();
 
-                assert.equal(_.size(interactions), 1, 'one interaction');
-                interaction = interactions[0];
+                    assert.equal(_.size(interactions), 1, 'one interaction');
+                    interaction = interactions[0];
 
-                //set the response
-                interaction.setResponse(response);
-            })
-            .on('responsechange', function (res){
-                changeCounter++;
-                if (changeCounter === 1) { // so it runs only once
-                    assert.ok(_.isPlainObject(res), 'response changed');
-                    assert.ok(_.isPlainObject(res.RESPONSE), 'response identifier ok');
-                    assert.deepEqual(res.RESPONSE, response, 'response set/get ok');
-                    QUnit.start();
-                }
-            })
-            .on('error', function (error){
-                $('#error-display').html(error);
-            })
-            .init()
-            .render($container);
+                    //set the response
+                    interaction.setResponse(response);
+                })
+                .on('responsechange', function (res){
+                    changeCounter++;
+                    if (changeCounter === 1) { // so it runs only once
+                        assert.ok(_.isPlainObject(res), 'response changed');
+                        assert.ok(_.isPlainObject(res.RESPONSE), 'response identifier ok');
+                        assert.deepEqual(res.RESPONSE, response, 'response set/get ok');
+                        QUnit.start();
+                    }
+                })
+                .on('error', function (error){
+                    $('#error-display').html(error);
+                })
+                .init()
+                .render($container);
+        }
     });
 
     /* */
@@ -200,24 +218,26 @@ define([
         assert.equal($container.length, 1, 'the item container exists');
         assert.equal($container.children().length, 0, 'the container has no children');
 
-        runner = qtiItemRunner('qti', itemData)
-            .on('render', function (){
+        if (supportsMediaRecorder()) {
+            runner = qtiItemRunner('qti', itemData)
+                .on('render', function (){
 
-                this.setState(state);
-                assert.deepEqual(this.getState(), state, 'state set/get ok');
-            })
-            .on('responsechange', function (res){
-                changeCounter++;
-                if (changeCounter === 1) { // so it runs only once
-                    assert.ok(_.isPlainObject(res), 'response changed');
-                    assert.ok(_.isPlainObject(res.RESPONSE), 'response identifier ok');
-                    assert.deepEqual(res, state, 'response set/get ok');
+                    this.setState(state);
+                    assert.deepEqual(this.getState(), state, 'state set/get ok');
+                })
+                .on('responsechange', function (res){
+                    changeCounter++;
+                    if (changeCounter === 1) { // so it runs only once
+                        assert.ok(_.isPlainObject(res), 'response changed');
+                        assert.ok(_.isPlainObject(res.RESPONSE), 'response identifier ok');
+                        assert.deepEqual(res, state, 'response set/get ok');
 
-                    QUnit.start();
-                }
-            })
-            .init()
-            .render($container);
+                        QUnit.start();
+                    }
+                })
+                .init()
+                .render($container);
+        }
     });
 
     /* */
@@ -229,19 +249,23 @@ define([
         var $container = $('#outside-container');
         assert.equal($container.length, 1, 'the item container exists');
 
-        runner = qtiItemRunner('qti', itemData)
-            .on('render', function (){
-                QUnit.start();
-            })
-            .on('responsechange', function (response){
-                $('#response-display').html(JSON.stringify(response, null, 2));
-            })
-            .on('error', function (error){
-                $('#error-display').html(error);
-            })
-            .init()
-            .render($container);
+        if (supportsMediaRecorder()) {
+            runner = qtiItemRunner('qti', itemData)
+                .on('render', function (){
+                    QUnit.start();
+                })
+                .on('responsechange', function (response){
+                    $('#response-display').html(JSON.stringify(response, null, 2));
+                })
+                .on('error', function (error){
+                    $('#error-display').html(error);
+                })
+                .init()
+                .render($container);
+        }
     });
+
+     /* */
 
 });
 
