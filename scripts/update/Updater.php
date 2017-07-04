8ef4ad43f23a290376466262eb96e1bb92aa000d
Sam Sipasseuth <sam@taotesting.com>
2017-07-04 18:53:21 +0200

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -145,6 +145,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('2.1.0');
         }
 
-        $this->skip('2.1.0', '2.2.0');
+        $this->skip('2.1.0', '2.2.1');
     }
 }
