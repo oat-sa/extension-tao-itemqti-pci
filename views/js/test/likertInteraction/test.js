9822cc7c98a36401b3a480a95e97d2f33275aaed
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2018-05-25 13:30:00 +0200

    fix the likertscale test

--- views/js/test/likertInteraction/test.js
+++ views/js/test/likertInteraction/test.js
@@ -26,8 +26,8 @@ define([
     'taoQtiItem/runner/qtiItemRunner',
     'taoQtiItem/portableElementRegistry/ciRegistry',
     'taoQtiItem/portableElementRegistry/provider/localManifestProvider',
-    'text!qtiItemPci/test/likertInteraction/data/likert_triple/qti.xml'
-], function ($, _, url, assetManagerFactory, assetStrategies, portableAssetStrategy, qtiItemRunner, ciRegistry, pciTestProvider, likertTripleXml) {
+    'json!qtiItemPci/test/likertInteraction/data/likert_triple/qti.json'
+], function ($, _, url, assetManagerFactory, assetStrategies, portableAssetStrategy, qtiItemRunner, ciRegistry, pciTestProvider, likertTripleData) {
 
     'use strict';
 
@@ -42,60 +42,41 @@ define([
         ], {baseUrl: baseUrl || ''});
     }
 
-    function parseXml(xml) {
-        return new Promise(function (resolve, reject) {
-            $.ajax({
-                    url: url.route('getJson', 'Parser', 'taoQtiItem'),
-                    type: 'POST',
-                    contentType: 'text/xml',
-                    dataType: 'json',
-                    data: xml
-                })
-                .done(function (response) {
-                    return resolve(response.itemData);
-                })
-                .fail(function (xhr) {
-                    return reject(new Error(xhr.status + ' : ' + xhr.statusText));
-                });
-        });
-    }
-
     //manually register the pci from its manifest
-    pciTestProvider.addManifestPath('likertInteraction', 'qtiItemPci/pciCreator/dev/likertInteraction/imsPciCreator.json');
+    pciTestProvider.addManifestPath('likertScaleInteraction', 'qtiItemPci/pciCreator/dev/likertScaleInteraction/pciCreator.json');
     ciRegistry.resetProviders();
     ciRegistry.registerProvider(pciTestProvider.getModuleName());
 
     QUnit.asyncTest('renders correctly', function (assert) {
 
+        var assetManager = getAssetManager('/qtiItemPci/views/js/test/likertInteraction/data/likert_triple/');
         var $container = $('#' + fixtureContainerId);
         assert.equal($container.length, 1, 'the item container exists');
         assert.equal($container.children().length, 0, 'the container has no children');
 
-        parseXml(likertTripleXml).then(function (itemData) {
-            var assetManager = getAssetManager('/qtiItemPci/views/js/test/likertInteraction/data/likert_triple/');
-            runner = qtiItemRunner('qti', itemData, {assetManager: assetManager})
-                .on('render', function () {
+        runner = qtiItemRunner('qti', likertTripleData, {assetManager: assetManager})
+            .on('render', function () {
 
-                    assert.equal($container.children().length, 1, 'the container a elements');
-                    assert.equal($container.children('.qti-item').length, 1, 'the container contains a the root element .qti-item');
-                    assert.equal($container.find('.qti-interaction').length, 3, 'the container contains 3 interactions .qti-interaction');
-                    assert.equal($container.find('.qti-interaction.qti-customInteraction').length, 3, 'the container contains 3 custom interactions');
-                    assert.equal($container.find('.qti-customInteraction .likertInteraction').length, 3, 'the container contains 3 likert interactions');
-                    assert.equal($container.find('.qti-customInteraction .prompt').length, 3, 'the interaction contains 3 prompts');
+                assert.equal($container.children().length, 1, 'the container a elements');
+                assert.equal($container.children('.qti-item').length, 1, 'the container contains a the root element .qti-item');
+                assert.equal($container.find('.qti-interaction').length, 3, 'the container contains 3 interactions .qti-interaction');
+                assert.equal($container.find('.qti-interaction.qti-customInteraction').length, 3, 'the container contains 3 custom interactions');
+                assert.equal($container.find('.qti-customInteraction .likertScaleInteraction').length, 3, 'the container contains 3 likert interactions');
+                assert.equal($container.find('.qti-customInteraction .prompt').length, 3, 'the interaction contains 3 prompts');
 
-                    QUnit.start();
-                    runner.clear();
-                })
-                .on('error', function (error) {
-                    $('#error-display').html(error);
-                })
-                .init()
-                .render($container);
-        });
+                QUnit.start();
+                runner.clear();
+            })
+            .on('error', function (error) {
+                $('#error-display').html(error);
+            })
+            .init()
+            .render($container);
     });
 
     QUnit.asyncTest('state standard', function (assert) {
 
+        var assetManager = getAssetManager('/qtiItemPci/views/js/test/likertInteraction/data/likert_triple/');
         var $container = $('#' + fixtureContainerId);
         var state1 = {response: {base: {integer: 2}}};
         var state2 = {response: {base: {integer: 5}}};
@@ -103,36 +84,31 @@ define([
         assert.equal($container.length, 1, 'the item container exists');
         assert.equal($container.children().length, 0, 'the container has no children');
 
+        runner = qtiItemRunner('qti', likertTripleData, {assetManager: assetManager})
+            .on('render', function () {
 
-        parseXml(likertTripleXml).then(function (itemData) {
-            var assetManager = getAssetManager('/qtiItemPci/views/js/test/likertInteraction/data/likert_triple/');
-            runner = qtiItemRunner('qti', itemData, {assetManager: assetManager})
-                .on('render', function () {
+                assert.equal($('input:radio[name=likert1]:checked').val(), '2', 'likert 1 state set');
+                assert.equal($('input:radio[name=likert2]:checked').val(), '5', 'likert 2 state set');
+                assert.equal($('input:radio[name=likert3]:checked').val(), undefined, 'likert 3 state untouched');
 
-                    assert.equal($('input:radio[name=likert1]:checked').val(), '2', 'likert 1 state set');
-                    assert.equal($('input:radio[name=likert2]:checked').val(), '5', 'likert 2 state set');
-                    assert.equal($('input:radio[name=likert3]:checked').val(), undefined, 'likert 3 state untouched');
+                assert.deepEqual(this.getState(), {
+                    likert1: state1,
+                    likert2: state2,
+                    likert3: {response: {base: {integer: 0}}},
+                }, 'state ok');
 
-                    assert.deepEqual(this.getState(), {
-                        likert1: state1,
-                        likert2: state2,
-                        likert3: {response: {base: {integer: 0}}},
-                    }, 'state ok');
-
-                    QUnit.start();
-                })
-                .on('error', function (error) {
-                    $('#error-display').html(error);
-                })
-                .init()
-                .render($container, {
-                    state : {
-                        likert1: state1,
-                        likert2: state2
-                    }
-                });
-        });
+                QUnit.start();
+            })
+            .on('error', function (error) {
+                $('#error-display').html(error);
+            })
+            .init()
+            .render($container, {
+                state : {
+                    likert1: state1,
+                    likert2: state2
+                }
+            });
     });
-
 });
 
