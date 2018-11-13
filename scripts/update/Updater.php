480263b00e4f5f15638f7b2bbf74f9af488e2273
Péter Halász <halaszpeterzsolt@gmail.com>
2018-11-13 15:23:16 +0100

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -315,6 +315,10 @@ class Updater extends \common_ext_ExtensionUpdater
             call_user_func(new RegisterPciAudioRecording(), ['0.4.0']);
             $this->setVersion('4.6.0');
         }
-        $this->skip('4.6.0', '4.6.5');
+        $this->skip('4.6.0', '4.7.5');
+
+        if ($this->isVersion('4.7.5')) {
+            call_user_func(new RegisterPciAudioRecording(), ['0.5.0']);
+        }
     }
 }
