90047b959311d1eb4df533a8dcede958615ba39f
Péter Halász <halaszpeterzsolt@gmail.com>
2018-11-15 10:07:49 +0100

    fix wav encoder parameters

--- views/js/pciCreator/dev/audioRecordingInteraction/runtime/js/workers/WavAudioEncoder.js
+++ views/js/pciCreator/dev/audioRecordingInteraction/runtime/js/workers/WavAudioEncoder.js
@@ -35,8 +35,8 @@
     /**
      * @returns {DataView}
      */
-    var getDataView = function() {
-        var dataSize = this.numChannels * this.numSamples * 2,
+    var getDataView = function(numChannels, numSamples, sampleRate) {
+        var dataSize = numChannels * numSamples * 2,
             view     = new DataView(new ArrayBuffer(44));
         setString(view, 0, 'RIFF');
         view.setUint32(4, 36 + dataSize, true);
@@ -44,10 +44,10 @@
         setString(view, 12, 'fmt ');
         view.setUint32(16, 16, true);
         view.setUint16(20, 1, true);
-        view.setUint16(22, this.numChannels, true);
-        view.setUint32(24, this.sampleRate, true);
-        view.setUint32(28, this.sampleRate * 4, true);
-        view.setUint16(32, this.numChannels * 2, true);
+        view.setUint16(22, numChannels, true);
+        view.setUint32(24, sampleRate, true);
+        view.setUint32(28, sampleRate * 4, true);
+        view.setUint16(32, numChannels * 2, true);
         view.setUint16(34, 16, true);
         setString(view, 36, 'data');
         view.setUint32(40, dataSize, true);
@@ -84,7 +84,7 @@
      * @returns {Blob}
      */
     Encoder.prototype.finish = function(mimeType) {
-        var view = getDataView();
+        var view = getDataView(this.numChannels, this.numSamples, this.sampleRate);
         this.dataViews.unshift(view);
         var blob = new Blob(this.dataViews, { type: 'audio/wav' });
         this.cleanup();
@@ -98,7 +98,7 @@
      * @returns {Blob}
      */
     Encoder.prototype.partialFinish = function(mimeType) {
-        var view = getDataView();
+        var view = getDataView(this.numChannels, this.numSamples, this.sampleRate);
         var tmpDataViews = this.dataViews.slice();
         tmpDataViews.unshift(view);
         var blob = new Blob(tmpDataViews, { type: 'audio/wav' });
