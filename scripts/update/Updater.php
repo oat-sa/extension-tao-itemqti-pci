d5798706008eddf2dbe58283363de11437f5f893
Ihar Suleimanau <igor.salevan@gmail.com>
2019-04-26 12:02:54 +0300

    TAO-7632: Bumped version.

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -360,6 +360,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('5.3.0');
         }
 
-        $this->skip('5.3.0', '5.3.1');
+        $this->skip('5.3.0', '5.3.2');
     }
 }
