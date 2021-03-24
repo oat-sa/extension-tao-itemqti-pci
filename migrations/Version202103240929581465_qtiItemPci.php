e97aca3d7567aa9f850bbe38373c032549cd1915
Anshul Sharma <assharmaanshul119@gmail.com>
2021-03-24 10:49:48 +0100

    chore: add migration

---
+++ migrations/Version202103240929581465_qtiItemPci.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace oat\qtiItemPci\migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use oat\tao\scripts\tools\migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version202103240929581465_qtiItemPci extends AbstractMigration
+{
+
+    public function getDescription(): string
+    {
+        return 'Added support for review mode for Audio Recording interaction';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $registry = (new PciModel())->getRegistry();
+        if ($registry->has('audioRecordingInteraction')) {
+            /** @noinspection PhpUnhandledExceptionInspection */
+            $registry->removeAllVersions('audioRecordingInteraction');
+        }
+
+        $this->addReport(
+            $this->propagate(
+                new RegisterPciAudioRecording()
+            )(
+                ['0.13.0']
+            )
+        );
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        throw new IrreversibleMigration(
+            'In order to undo this migration, please revert the client-side changes and run ' . RegisterPciAudioRecording::class
+        );
+    }
+}
