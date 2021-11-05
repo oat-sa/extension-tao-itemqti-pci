8c7f9cdc70bdc62a33cac524654e0c92cd4169f4
Yuriy Synyaiev <yuriy.synyaiev@taotesting.com>
2021-11-05 12:34:38 +0100

    feat: add migration script

---
+++ migrations/Version202111051126451465_qtiItemPci.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace oat\qtiItemPci\migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\Exception\IrreversibleMigration;
+use oat\tao\scripts\tools\migrations\AbstractMigration;
+use oat\qtiItemPci\scripts\install\RegisterPciLikertScale;
+use oat\qtiItemPci\model\PciModel;
+
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version202111051126451465_qtiItemPci extends AbstractMigration
+{
+
+    public function getDescription(): string
+    {
+        return 'Update Likert Scale interaction';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $registry = (new PciModel())->getRegistry();
+        if ($registry->has('likertScaleInteraction')) {
+            /** @noinspection PhpUnhandledExceptionInspection */
+            $registry->removeAllVersions('likertScaleInteraction');
+        }
+
+        $this->addReport(
+            $this->propagate(
+                new RegisterPciLikertScale()
+            )(
+                ['0.6.0']
+            )
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        throw new IrreversibleMigration(
+            'In order to undo this migration, please revert the client-side changes and run ' . RegisterPciLikertScale::class
+        );
+    }
+}
