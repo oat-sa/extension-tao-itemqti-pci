875cd9a24d78a8184a402b324d96ea712b753aae
Silvia_Lozano <sylvilozano@gmail.com>
2022-03-24 13:20:27 +0100

    fix: audio pci controls fliped for RTL

---
+++ migrations/Version202203241158531465_qtiItemPci.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace oat\qtiItemPci\migrations;
+use Doctrine\DBAL\Schema\Schema;
+use oat\tao\scripts\tools\migrations\AbstractMigration;
+use Doctrine\Migrations\Exception\IrreversibleMigration;
+use oat\qtiItemPci\scripts\install\RegisterPciAudioRecording;
+use oat\qtiItemPci\model\PciModel;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version202203241158531465_qtiItemPci extends AbstractMigration
+{
+
+    public function getDescription(): string
+    {
+        return 'Update Audio Recording interaction with RTL display requirements';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $registry = (new PciModel())->getRegistry();
+        if ($registry->has('audioRecordingInteraction')) {
+            /** @noinspection PhpUnhandledExceptionInspection */
+            $registry->removeAllVersions('audioRecordingInteraction');
+        }
+
+        $this->addReport(
+            $this->propagate(
+                new RegisterPciAudioRecording()
+            )(
+                ['0.14.3']
+            )
+        );
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        throw new IrreversibleMigration(
+            'In order to undo this migration, please revert the client-side changes and run ' . RegisterPciAudioRecording::class
+        );
+
+    }
+}
