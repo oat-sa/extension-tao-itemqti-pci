eec32e48c4d9ad6a1fea482fbc367b5fc2c28d7c
jsconan <jsconan@gmail.com>
2022-09-14 12:51:20 +0200

    doc: update the copyright year

--- views/js/pciCreator/ims/mathEntryInteraction/runtime/mathEntryInteraction.js
+++ views/js/pciCreator/ims/mathEntryInteraction/runtime/mathEntryInteraction.js
@@ -13,10 +13,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2016-2021 (original work) Open Assessment Technologies SA;
+ * Copyright (c) 2016-2022 (original work) Open Assessment Technologies SA;
  *
  * @author Christophe Noël <christophe@taotesting.com>
- *
  */
 define([
     'qtiCustomInteractionContext',
