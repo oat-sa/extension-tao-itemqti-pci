fa31d3e44c2de3cce871ec1ca2a5a06b3168dea9
maria.cano <maria.cano@taotesting.com>
2022-11-03 11:52:19 +0100

    fix: migration file and bump version

---
+++ migrations/Version202211031046021465QtiItemPci.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace oat\qtiItemPci\migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\Exception\IrreversibleMigration;
+use oat\qtiItemPci\scripts\install\RegisterPciMathEntry;
+use oat\tao\scripts\tools\migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version202211031046021465QtiItemPci extends AbstractMigration
+{
+
+    public function getDescription(): string
+    {
+        return 'Update the Math Entry PCI, set response mapping default to 0 for incorrect responses';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addReport(
+            $this->propagate(
+                new RegisterPciMathEntry()
+            )(
+                ['2.4.1']
+            )
+        );
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        throw new IrreversibleMigration(
+            'In order to undo this migration, please revert the client-side changes and run ' . RegisterPciMathEntry::class
+        );
+
+    }
+}
