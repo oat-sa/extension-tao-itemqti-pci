ef67386d519dbc708fb76c82198e15a4331f55f8
jsconan <jsconan@gmail.com>
2023-09-08 13:45:19 +0200

    fix: replace @qtikit/mathml-to-latex with mathml-to-latex

--- views/js/pciCreator/ims/mathEntryInteraction/rollup.config.js
+++ views/js/pciCreator/ims/mathEntryInteraction/rollup.config.js
@@ -2,7 +2,7 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import commonJS from '@rollup/plugin-commonjs';
 
 const dependencies = [{
-    src: 'node_modules/@qtikit/mathml-to-latex/dist/index.js',
+    src: 'node_modules/mathml-to-latex/dist/bundle.min.js',
     dst: 'runtime/mathml-to-latex/mathml-to-latex.js',
     name: 'Mathml2latex'
 }]
