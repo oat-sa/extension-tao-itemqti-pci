3bfa39cdd93401d380c4bc5676f5b5e77bb23d52
Martin Nicholson <martin@taotesting.com>
2024-02-23 16:14:06 +0100

    feat: add creator part, with new markup & form

--- views/js/pciCreator/ims/likertScaleInteraction/creator/widget/Widget.js
+++ views/js/pciCreator/ims/likertScaleInteraction/creator/widget/Widget.js
@@ -1,15 +1,38 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2016-2024 (original work) Open Assessment Technologies SA;
+ *
+ */
 define([
     'taoQtiItem/qtiCreator/widgets/interactions/customInteraction/Widget',
-    'likertScaleInteraction/creator/widget/states/states',
-], function (Widget, states) {
+    'likertScaleInteraction/creator/widget/states/states'
+], function(Widget, states){
     'use strict';
 
-    const interactionWidget = Widget.clone();
+    var likertScaleInteractionWidget = Widget.clone();
 
-    interactionWidget.initCreator = function () {
+    likertScaleInteractionWidget.initCreator = function(){
+
         this.registerStates(states);
+
         Widget.initCreator.call(this);
+
+        //for existing likert scale PCI, ensure that the rp template is always NONE
+        this.element.getResponseDeclaration().setTemplate('NONE');
     };
 
-    return interactionWidget;
+    return likertScaleInteractionWidget;
 });
--- views/js/pciCreator/ims/likertScaleInteraction/creator/widget/states/Question.js
+++ views/js/pciCreator/ims/likertScaleInteraction/creator/widget/states/Question.js
@@ -1,43 +1,118 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2016-2024 (original work) Open Assessment Technologies SA;
+ *
+ */
 define([
     'taoQtiItem/qtiCreator/widgets/states/factory',
     'taoQtiItem/qtiCreator/widgets/interactions/states/Question',
     'taoQtiItem/qtiCreator/widgets/helpers/formElement',
-], function(stateFactory, Question, formElement) {
+    'taoQtiItem/qtiCreator/editor/simpleContentEditableElement',
+    'taoQtiItem/qtiCreator/editor/containerEditor',
+    'tpl!likertScaleInteraction/creator/tpl/propertiesForm',
+    'lodash',
+    'jquery'
+], function(stateFactory, Question, formElement, simpleEditor, containerEditor, formTpl, _){
     'use strict';
 
-    var stateQuestion = stateFactory.extend(
-        Question,
-        function enterQuestionState() {
-            //
-        },
-        function leaveQuestionState() {
-            //
-        }
-    );
-
-    /**
-     * Setup the property panel
-     */
-    stateQuestion.prototype.initForm = function() {
+    const LikertInteractionStateQuestion = stateFactory.extend(Question, function(){
+        const $container = this.widget.$container;
+        const $prompt = $container.find('.prompt');
         const interaction = this.widget.element;
-        const $form = this.widget.$form;
+
+        containerEditor.create($prompt, {
+            change : function change(text){
+                interaction.data('prompt', text);
+                interaction.updateMarkup();
+            },
+            markup : interaction.markup,
+            markupSelector : '.prompt',
+            related : interaction,
+            areaBroker: this.widget.getAreaBroker()
+        });
+
+        simpleEditor.create($container, '.likert-label-min', function(text) {
+            interaction.prop('label-min', text);
+        });
+
+        simpleEditor.create($container, '.likert-label-max', function(text) {
+            interaction.prop('label-max', text);
+        });
+
+    }, function destroy() {
+        const $container = this.widget.$container;
+        const $prompt = $container.find('.prompt');
+
+        simpleEditor.destroy(this.widget.$container);
+        containerEditor.destroy($prompt);
+    });
+
+    LikertInteractionStateQuestion.prototype.initForm = function(){
+        const _widget = this.widget;
+        const $form = _widget.$form;
+        const interaction = _widget.element;
+        const response = interaction.getResponseDeclaration();
+
+        const level = parseInt(interaction.prop('level')) || 5;
+        const levels = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+        const levelData = {};
+
+        const icons = ![false, 'false'].includes(interaction.prop('icons'));
+        const numbers = ![false, 'false'].includes(interaction.prop('numbers'));
+
+        //build select option data for the template
+        levels.forEach(lvl => {
+            levelData[lvl] = {
+                label: lvl,
+                selected: lvl === level
+            };
+        });
 
         //render the form using the form template
-        this.widget.$form.html(
-            //some HTML
-        );
+        $form.html(formTpl({
+            serial: response.serial,
+            levels: levelData,
+            icons,
+            numbers
+        }));
 
         //init form javascript
-        formElement.initWidget(this.widget.$form);
+        formElement.initWidget($form);
 
         //init data change callbacks
         formElement.setChangeCallbacks($form, interaction, {
-            //example
-            // propertyName: function(i, value){
-            //    i.properties[propertyName] = value;
-            // }
+            level: function(interaction, value) {
+                //update the pci property value:
+                interaction.prop('level', value);
+
+                //trigger change event:
+                interaction.triggerPci('configChange', [{ level: parseInt(value) }]);
+            },
+
+            icons: function(interaction, value) {
+                interaction.prop('icons', value);
+                interaction.triggerPci('configChange', [{ icons: value }]);
+            },
+
+            numbers: function(interaction, value) {
+                interaction.prop('numbers', value);
+                interaction.triggerPci('configChange', [{ numbers: value }]);
+            }
         });
     };
-    return stateQuestion;
-});
 
+    return LikertInteractionStateQuestion;
+});
