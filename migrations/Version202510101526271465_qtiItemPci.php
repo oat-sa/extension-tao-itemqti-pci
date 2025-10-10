e8b142faee020e4e23c0b93c10d6f6d1aafbada0
KirylHatalski <kirill.gatalsky@taotesting.com>
2025-10-10 18:36:04 +0300

    fix: fix for disappearing prompts formulas

---
+++ migrations/Version202510101526271465_qtiItemPci.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace oat\qtiItemPci\migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use oat\tao\scripts\tools\migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ *
+ * phpcs:disable Squiz.Classes.ValidClassName
+ */
+final class Version202510101526271465_qtiItemPci extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
