b1b35df5c9ca96e1e473b57e3e08bf3b1b54506b
KirylHatalski <kirill.gatalsky@taotesting.com>
2025-10-15 13:51:20 +0300

    fix: fix migration for MathEntry

--- migrations/Version202510101544391465_qtiItemPci.php
+++ migrations/Version202510101544391465_qtiItemPci.php
@@ -6,6 +6,8 @@ namespace oat\qtiItemPci\migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use oat\tao\scripts\tools\migrations\AbstractMigration;
+use Doctrine\Migrations\Exception\IrreversibleMigration;
+use oat\qtiItemPci\scripts\install\RegisterPciMathEntry;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
