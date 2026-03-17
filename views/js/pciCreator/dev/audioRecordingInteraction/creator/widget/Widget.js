61731d4047a4b95e83531ce23e7c0f28307b6de6
Bartłomiej Marszał <bartlomiejmarszal@gmail.com>
2026-03-17 09:31:54 +0100

    chore: update audio PCI license headers

--- views/js/pciCreator/dev/audioRecordingInteraction/creator/widget/Widget.js
+++ views/js/pciCreator/dev/audioRecordingInteraction/creator/widget/Widget.js
@@ -11,9 +11,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 31 Milk Street, # 960789, Boston, MA 02196, USA.
  *
- * Copyright (c) 2017 (original work) Open Assessment Technologies SA;
+ * Copyright (c) 2017-2026 (original work) Open Assessment Technologies SA;
  */
 define([
     'taoQtiItem/qtiCreator/widgets/interactions/customInteraction/Widget',
