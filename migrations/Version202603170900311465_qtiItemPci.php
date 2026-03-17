50c9e2cacaed05f4676baa52033f86043497bcc1
Bartłomiej Marszał <bartlomiejmarszal@gmail.com>
2026-03-17 09:18:20 +0100

    chore: add migration for audio recording PCI response mode

---
+++ migrations/Version202603170900311465_qtiItemPci.php
@@ -0,0 +1,43 @@
+
+<?php
+
+declare(strict_types=1);
+
+namespace oat\qtiItemPci\migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\Exception\IrreversibleMigration;
+use oat\qtiItemPci\scripts\install\RegisterIMSPciAudioRecording;
+use oat\tao\scripts\tools\migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ *
+ * phpcs:disable Squiz.Classes.ValidClassName
+ */
+final class Version202603170900311465_qtiItemPci extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Update Audio Recording PCI: enable response mode response properties';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addReport(
+            $this->propagate(
+                new RegisterIMSPciAudioRecording()
+            )(
+                ['1.3.4']
+            )
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        throw new IrreversibleMigration(
+            'In order to undo this migration, please revert the client-side changes and run '
+            . RegisterIMSPciAudioRecording::class
+        );
+    }
+}
