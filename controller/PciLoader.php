3c2392ae04af80597921bda8fd6b1c287316ac06
Martin Nicholson <martin@taotesting.com>
2026-03-30 15:24:56 +0200

    refactor: change PciLoader->load into a thin wrapper, move implementation to a separate method for internal calls

--- controller/PciLoader.php
+++ controller/PciLoader.php
@@ -29,79 +29,88 @@ use tao_actions_CommonModule;
 class PciLoader extends tao_actions_CommonModule
 {
     /**
-     * Load latest PCI runtimes
+     * Load latest PCI runtimes (controller action)
      */
     public function load()
+    {
+        try {
+            $result = $this->getLatestPciRuntimes();
+            $this->returnJson($result);
+        } catch (PortableElementException $e) {
+            $this->returnJson($e->getMessage(), 500);
+        }
+    }
+
+    /**
+     * Get latest PCI runtimes data
+     *
+     * @return array
+     */
+    public function getLatestPciRuntimes(): array
     {
         $customInteractionDirs = \common_ext_ExtensionsManager::singleton()
             ->getExtensionById('taoQtiItem')
             ->getConfig('debug_portable_element');
 
-        try {
-            $result = array_reduce($this->getRegistries(), function ($acc, $registry) use ($customInteractionDirs) {
-                $latestRuntimes = $registry->getLatestRuntimes();
-                if (is_array($customInteractionDirs)) {
-                    foreach ($latestRuntimes as $id => &$versions) {
-                        if (isset($customInteractionDirs[$id])) {
-                            //add option to load from source (as opposed to from the default min.js files)
-                            foreach ($versions as &$version) {
-                                // IMS PCI
-                                if (isset($version['model']) && $version['model'] == 'IMSPCI') {
-                                    $modules = (isset($version['runtime']) && isset($version['runtime']['modules']))
-                                        ? $version['runtime']['modules']
-                                        : [];
-                                    $src = (isset($version['runtime']) && isset($version['runtime']['src']))
-                                        ? $version['runtime']['src']
-                                        : [];
+        return array_reduce($this->getRegistries(), function ($acc, $registry) use ($customInteractionDirs) {
+            $latestRuntimes = $registry->getLatestRuntimes();
+            if (is_array($customInteractionDirs)) {
+                foreach ($latestRuntimes as $id => &$versions) {
+                    if (isset($customInteractionDirs[$id])) {
+                        //add option to load from source (as opposed to from the default min.js files)
+                        foreach ($versions as &$version) {
+                            // IMS PCI
+                            if (isset($version['model']) && $version['model'] == 'IMSPCI') {
+                                $modules = (isset($version['runtime']) && isset($version['runtime']['modules']))
+                                    ? $version['runtime']['modules']
+                                    : [];
+                                $src = (isset($version['runtime']) && isset($version['runtime']['src']))
+                                    ? $version['runtime']['src']
+                                    : [];
 
-                                    // in case of a TAO bundled PCI (= we have a "src" entry),
-                                    // we redirect the module to the entry point of the PCI instead of its minified
-                                    // version
-                                    if (count($src) > 0) {
-                                        foreach ($modules as $moduleKey => &$allModulesFiles) {
-                                            if (strpos($moduleKey, '.min') == (strlen($moduleKey) - strlen('.min'))) {
-                                                foreach ($allModulesFiles as &$moduleFile) {
-                                                    $moduleFile = str_replace('.min.js', '.js', $moduleFile);
-                                                }
+                                // in case of a TAO bundled PCI (= we have a "src" entry),
+                                // we redirect the module to the entry point of the PCI instead of its minified
+                                // version
+                                if (count($src) > 0) {
+                                    foreach ($modules as $moduleKey => &$allModulesFiles) {
+                                        if (strpos($moduleKey, '.min') == (strlen($moduleKey) - strlen('.min'))) {
+                                            foreach ($allModulesFiles as &$moduleFile) {
+                                                $moduleFile = str_replace('.min.js', '.js', $moduleFile);
                                             }
                                         }
                                     }
+                                }
 
-                                    if (isset($version['runtime']) && isset($version['runtime']['modules'])) {
-                                        $version['runtime']['modules'] = $modules;
-                                    }
-                                    if (isset($version['creator']) && isset($version['creator']['src'])) {
-                                        //the first entry of the src array is always the entrypoint (PCI hook)
-                                        $version['creator']['hook'] = array_shift($version['creator']['src']);
-                                        $version['creator']['modules'] = $version['creator']['src'];
-                                        unset($version['creator']['src']);
-                                    }
+                                if (isset($version['runtime']) && isset($version['runtime']['modules'])) {
+                                    $version['runtime']['modules'] = $modules;
+                                }
+                                if (isset($version['creator']) && isset($version['creator']['src'])) {
+                                    //the first entry of the src array is always the entrypoint (PCI hook)
+                                    $version['creator']['hook'] = array_shift($version['creator']['src']);
+                                    $version['creator']['modules'] = $version['creator']['src'];
+                                    unset($version['creator']['src']);
+                                }
 
-                                    // TAO PCI
-                                } else {
-                                    if (isset($version['runtime']) && isset($version['runtime']['src'])) {
-                                        $version['runtime']['libraries'] = $version['runtime']['src'];
-                                        unset($version['runtime']['hook']);
-                                        unset($version['runtime']['src']);
-                                    }
-                                    if (isset($version['creator']) && isset($version['creator']['src'])) {
-                                        //the first entry of the src array is always the entrypoint (PCI hook)
-                                        $version['creator']['hook'] = array_shift($version['creator']['src']);
-                                        $version['creator']['modules'] = $version['creator']['src'];
-                                        unset($version['creator']['src']);
-                                    }
+                                // TAO PCI
+                            } else {
+                                if (isset($version['runtime']) && isset($version['runtime']['src'])) {
+                                    $version['runtime']['libraries'] = $version['runtime']['src'];
+                                    unset($version['runtime']['hook']);
+                                    unset($version['runtime']['src']);
+                                }
+                                if (isset($version['creator']) && isset($version['creator']['src'])) {
+                                    //the first entry of the src array is always the entrypoint (PCI hook)
+                                    $version['creator']['hook'] = array_shift($version['creator']['src']);
+                                    $version['creator']['modules'] = $version['creator']['src'];
+                                    unset($version['creator']['src']);
                                 }
                             }
                         }
                     }
                 }
-                return array_merge($acc, $latestRuntimes);
-            }, []);
-
-            $this->returnJson($result);
-        } catch (PortableElementException $e) {
-            $this->returnJson($e->getMessage(), 500);
-        }
+            }
+            return array_merge($acc, $latestRuntimes);
+        }, []);
     }
 
     protected function getRegistries()
